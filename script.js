56a0c27b5a80e76c1467e7ebcbd27b1274c36f58
nguyenlinhd <123815150+nguyenlinhd@users.noreply.github.com>
2024-12-06 18:11:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
     const circle = document.querySelector('.circle');
-    
+    const musicLogo = document.querySelector('.music-logo');
+    const musicPlayer = document.getElementById('music-player');
+
     circle.addEventListener('mouseover', function() {
         this.style.animation = 'rotate 2s linear infinite';
     });
-    
+
     circle.addEventListener('mouseout', function() {
         this.style.animation = 'rotate 3s linear infinite';
+    });
+
+    musicLogo.addEventListener('click', function() {
+        if (musicPlayer.classList.contains('hidden')) {
+            musicPlayer.classList.remove('hidden');
+        } else {
+            musicPlayer.classList.add('hidden');
+        }
     });
 });
